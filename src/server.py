b715f5ef8b6b040a3dd4d39f3c1b2528bd78d57a
IvanIsak2000 <vingving648@gmail.com>
2023-06-04 22:21:32 +0300

    update

--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI()
 app.mount('/images', StaticFiles(directory='images'), name='images')
@@ -43,6 +44,22 @@ async def get_request_with_token(TOKEN):
 
 
  
-@app.post("/postdata")
-def postdata(username = Form(), userage=Form()):
-    return print({"name": username, "age": userage})
+@app.post("/log_in/authorization")
+def postdata(username=Form(), password=Form()):
+    try:
+        fake_db = {'Iwan':'123'}
+
+        print({"name": username, "password": password})
+
+    
+        if fake_db[username] == password:
+            return HTMLResponse('Welcome!')
+        
+        else :
+            return HTMLResponse('False')
+    except Exception as err:
+        return HTMLResponse('False')   
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=5049)
