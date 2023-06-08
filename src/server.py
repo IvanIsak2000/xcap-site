71d00c53480082fabb320c77479590d22825552f
IvanIsak2000 <vingving648@gmail.com>
2023-06-08 14:11:18 +0300

    update account render

--- src/server.py
+++ src/server.py
@@ -46,45 +46,48 @@ async def get_request_with_token(TOKEN):
 
  
 @app.post("/log_in/authorization/", response_class=HTMLResponse)
-def postdata(username=Form(), password=Form()):
-        
-    fake_db = ['96d5dbb402b62d37e8e39c77c110e5c9']
-    account_hash = hashlib.md5(b'Iwan123').hexdigest()
-    print(account_hash)
-
-    if account_hash in fake_db:
+def authorization(username=Form(), password=Form()):
+    fake_db = {
+        '96d5dbb402b62d37e8e39c77c110e5c9' :{
+            'username':'Iwan',
+            'settings': 'Empty',
+            'API_KEY' : '123'}
+    }
+    hashed_account_data = hashlib.md5(bytes(username + password, encoding='utf-8')).hexdigest()
+    if hashed_account_data in fake_db:
         return '''
         <!DOCTYPE html>
         <html>
-        <head>
-        <link rel="icon" href="images/logo.svg" type="image/icon type">
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>xcap</title>
-
-        <link rel="stylesheet" href="account.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+            <head>
+                <link rel="icon" href="images/logo.svg" type="image/icon type">
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>xcap</title>
+                <link rel="stylesheet" href="./account.css">
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+            </head>
+            <body>
+                <ul>
+                    <li>Username: {username}</li>
+                    <li>Settings: {settings}</a></li>
+                    <li>Exit</li>
+                </ul>
             
-        </head>
-        <body>
-
-        <ul>
-        <li>Username: {username}</li>
-        <li>Settings: {settings}</a></li>
-        <li>Your API_KEY: {API_KEY}</li>
-        <li>Exit</li>
-        </ul>
-
-
-        </body>
-        </html>
-
+            <div>
+                <p>Your API_KEY: {API_KEY}</p>
+            </div>
     
-            '''.format( username = username, settings = settings, API_KEY = API_KEY)
+            </body>
+         </html>'''.format(
+            username = fake_db[hashed_account_data]['username'],
+            settings = fake_db[hashed_account_data]['settings'],
+            API_KEY = fake_db[hashed_account_data]['API_KEY'])
+    else:
+        return 'False'
+
 
 if __name__ == "__main__":
-    #  uvicorn.run(app, host="127.0.0.1", port=5049)
     os.system('uvicorn server:app --reload')
